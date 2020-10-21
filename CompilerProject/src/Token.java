2c156d60b70fd2ecfef0c806c4036dab0f7be8dc
Arya Parvizi <aresparvizi@gmail.com>
2020-10-22 01:34:30 +0330

    list of all tokens in form of enumeration. needs furthure modification to operators and adding descriptions to it as well

--- CompilerProject/src/Token.java
+++ CompilerProject/src/Token.java
@@ -1,10 +1,41 @@
 public enum Token {
 
     //list of Tokens described in project.
+    /*==================================*/
+    //keywords:
+    T_void, T_int, T_double, T_bool, T_string, T_class, T_interface, T_null,
+    T_this, T_extends, T_implements, T_for, T_while, T_if, T_else, T_return,
+    T_break, T_continue, T_new, T_NewArray, T_Print, T_ReadInteger, T_ReadLine,
+    T_dtoi, T_itod, T_btoi, T_itob, T_private, T_protected, T_public,
+    /*==================================*/
+    //other reserved words:
+    T_BOOLEANLITERAL /*i.e. true | false*/,
+    /*==================================*/
+    // operators:
+    T_Plus, T_Minus, T_Star, T_Slash, T_Modulus,
+    T_GT, T_GE, T_LT, T_LE, T_Assign, T_EQ, T_NE,
+    T_AND, T_OR, T_NOT,
+    T_SemiColon, T_Comma, T_Dot,
+    T_OBracket, T_CBracket, T_OParen, T_Cparen,
+    /*==================================*/
+    // WHITE SPACE possible token for distinguisher. or just changes state.
+    T_STRINGLITERAL,
+    T_INTLITERAL,       // read descriptions // to be added
+    T_DOUBLELITERAL,    // read descriptions // to be added
+    T_ID; // all valid identifiers.
 
     /*private fields goes here*/
-    Token (/*desired input*/){
-        /*desired constructor*/
+    private String attribute = null;
+    private String givenName = null;
+
+    Token(){}
+    Token (String givenName){
+        this.givenName = givenName;
+    }
+
+    Token(String givenName, String attribute){
+        this.attribute = attribute;
+        this.givenName = givenName;
     }
 
     /*setters and getters.*/

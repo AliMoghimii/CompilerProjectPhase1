354d705ad869b11d3f6001eede90b286876cc9a8
Arya Parvizi <aresparvizi@gmail.com>
2020-10-22 00:43:03 +0330

    template for enums for Tokens

---
+++ CompilerProject/src/Token.java
@@ -0,0 +1,11 @@
+public enum Token {
+
+    //list of Tokens described in project.
+
+    /*private fields goes here*/
+    Token (/*desired input*/){
+        /*desired constructor*/
+    }
+
+    /*setters and getters.*/
+}
